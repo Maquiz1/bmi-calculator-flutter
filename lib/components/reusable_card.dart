b7dfe6a137dcd49f562b3fa79cb1b12ceee92335
Maquiz1 <manquiz92@gmail.com>
2021-09-14 16:50:07 +0300

    update active colours changes

--- lib/components/reusable_card.dart
+++ lib/components/reusable_card.dart
@@ -2,17 +2,19 @@ import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
 import '../screens/input_page.dart';
 
-
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.cardChild,this.onPress});
+  ReusableCard({
+    @required this.colour,
+    this.cardChild,
+    this.onPress,
+  });
 
-  final Color colour; //Keyword Final make property immutable
+  final Color colour;        //Keyword Final make property immutable
   final Widget cardChild;
   final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-
     // return Container(
     //   child: cardChild,
     //   margin: EdgeInsets.all(15.0),
@@ -28,7 +30,8 @@ class ReusableCard extends StatelessWidget {
         child: cardChild,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
-          color: kActiveCardColour,
+          // color: kActiveCardColour,
+          color: colour,
           borderRadius: BorderRadius.circular(10.0),
         ),
       ),
